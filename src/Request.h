2091585951fecd33c57e125c34fafe8a7866c839
Your Name <you@example.com>
2023-10-28 09:59:33 +0100

    adicionei bool de processado ou n

--- src/Request.h
+++ src/Request.h
@@ -10,10 +10,16 @@ class Request {
 private:
     static int count;  // Declare a static member variable for request ID.
     int requestId; //The ID that identifies each Request
+    bool processed;
+
 
 public:
     Request() {
         requestId = count++;
+        processed=0;
+    }
+    void setProcessed(bool processed){
+        this->processed=processed;
     }
 };
 
